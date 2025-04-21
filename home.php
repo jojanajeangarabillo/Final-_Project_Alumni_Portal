bb2f5d9bc6a90d524715440e426df9a2f77422dd
Jojana jean Garabillo <jojanajeangarabillo@gmail.com>
2025-04-21 17:13:11 +0800

    Manually resolved and updated home.php

--- home.php
+++ home.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,33 +28,3 @@
 
 </body>
 </html>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Academic Login - PLPasig</title>
-  <link rel="stylesheet" href="style.css" />
-  <link rel="stylesheet" href="login.css" />
-</head>
-<body>
-
-  <div class="login-container">
-    <div class="illustration">
-      <img src="plpasigg.jpg" alt="PLPasig Illustration">
-    </div>
-    <div class="login-form">
-      <h2>Log in</h2>
-      <form>
-        <input type="email" placeholder="Email" required />
-        <input type="password" placeholder="Password" required />
-        <button type="submit">Log in</button>
-      </form>
-      <p>Don't have an account? <a href="register.html">Register</a></p>
-    </div>
-  </div>
-
-</body>
-</html>
->>>>>>> 13552af1c0f9e20f627007c100ac4a95942a512e
